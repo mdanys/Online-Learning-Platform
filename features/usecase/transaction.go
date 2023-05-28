b8e6aec55b506fefbb2c1b61099cc542146353ea
Dany Perindo <dany@ndt-partners.com>
2023-05-28 17:50:18 +0700

    adding dockerfile ci/cd

--- features/usecase/transaction.go
+++ features/usecase/transaction.go
@@ -2,7 +2,6 @@ package usecase
 
 import (
 	"context"
-	"fmt"
 	"online-learning-platform/domain"
 
 	log "github.com/sirupsen/logrus"
@@ -21,13 +20,11 @@ func NewTransactionUsecase(transactionMySQLRepo domain.TransactionMySQLRepositor
 }
 
 func (tu *transactionUsecase) CreateTransaction(ctx context.Context, req domain.TransactionRequest) (transaction domain.Transaction, err error) {
-	fmt.Println(req.CourseID)
 	course, err := tu.courseMySQLRepo.SelectCourseByID(ctx, req.CourseID)
 	if err != nil {
 		log.Error(err)
 		return
 	}
-	fmt.Println(course)
 	req.Price = course.Price
 
 	id, err := tu.transactionMySQLRepo.InsertTransaction(ctx, req)
